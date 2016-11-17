e45fcb656ae795bae4514e2de6936c4fb92bbe03
aLIEz-sh <sh531801076@163.com>
2016-11-17 12:35:10 +0800

    chulizibiaoqian

---
+++ myscrapy2.py
@@ -0,0 +1,9 @@
+#chuli zibiaoqian
+from urllib import urlopen
+from bs4 import BeautifulSoup
+
+html=urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj=BeautifulSoup(html)
+
+for child in bsObj.find("table",{"id":"giftList"}).children:
+    print (child)
---
+++ myscrapy4.py
@@ -0,0 +1,7 @@
+#chuli fubiaoqian
+from urllib import urlopen
+from bs4 import BeautifulSoup
+
+html= urlopen("http://www.pythonscraping.com/pages/page3.html")
+bsObj=BeautifulSoup(html)
+print (bsObj.find("img",{"src":"../img/gifts/img1.jpg"}).parent.previous_sibling.get_text())
